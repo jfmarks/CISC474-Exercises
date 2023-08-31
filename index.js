00a1cf14eee70612dcdcdef5398eb03f2afaa4ee
jfmarks <124921997+jfmarks@users.noreply.github.com>
2023-08-31 12:28:17 -0400

    inputed name now displays on clicking "make it so"

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   $("form").submit(function(){
+    var firstName = $( "#fname" ).val();
+    var lastName = $( "#lname" ).val();
+    $("#jumbotron").text(firstName + " " + lastName);
     alert("Submitted");
+    return false;
   });
 });
